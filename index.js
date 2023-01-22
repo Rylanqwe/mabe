1e3caf9804feeb720a625fb734a9c0ebc4ffcae5
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-01-21 23:07:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let balance = 0;
+let balance = 100;
 const flipButton = document.getElementById("flip-button");
 const result = document.getElementById("result");
 const betAmount = document.getElementById("bet-amount");
@@ -7,7 +7,7 @@ const balanceDisplay = document.getElementById("balance");
 const message = document.getElementById("message");
 
 betAmount.addEventListener("input", function() {
-    if (betAmount.value > 0 && betSelection.value !== "") {
+    if (betAmount.value > 0 && betSelection.value !== "" && balance >= betAmount.value) {
         flipButton.disabled = false;
     } else {
         flipButton.disabled = true;
@@ -15,7 +15,7 @@ betAmount.addEventListener("input", function() {
 });
 
 betSelection.addEventListener("change", function() {
-    if (betAmount.value > 0 && betSelection.value !== "") {
+    if (betAmount.value > 0 && betSelection.value !== "" && balance >= betAmount.value) {
         flipButton.disabled = false;
     } else {
         flipButton.disabled = true;
@@ -26,4 +26,11 @@ flipButton.addEventListener("click", function() {
     let flip = Math.random();
     let heads = "Heads";
     let tails = "Tails";
-    let flipResult = flip < 0.5 ? heads : tails
+    let flipResult = flip < 0.5 ? heads : tails;
+    result.innerHTML = flipResult;
+    if (betSelection.value === flipResult) {
+        balance += parseInt(betAmount.value);
+        message.innerHTML = "You won!";
+    } else {
+        balance -= parseInt(betAmount.value);
+       
