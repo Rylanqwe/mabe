cf79083916073b19f9cca511c6d6070cf9b59ac2
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-01-21 23:17:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,37 +6,21 @@ const betSelection = document.getElementById("bet-selection");
 const balanceDisplay = document.getElementById("balance");
 const message = document.getElementById("message");
 
-betAmount.addEventListener("input", function() {
-    if (betAmount.value > 0 && betSelection.value !== "" && balance >= parseInt(betAmount.value)) {
-        flipButton.disabled = false;
-    } else {
-        flipButton.disabled = true;
-    }
-});
-
-betSelection.addEventListener("change", function() {
-    if (betAmount.value > 0 && betSelection.value !== "" && balance >= parseInt(betAmount.value)) {
-        flipButton.disabled = false;
-    } else {
-        flipButton.disabled = true;
-    }
-});
-
 flipButton.addEventListener("click", function() {
     let flip = Math.random();
     let heads = "Heads";
     let tails = "Tails";
     let flipResult = flip < 0.5 ? heads : tails;
     result.innerHTML = flipResult;
-    if (betSelection.value === flipResult) {
-        balance += parseInt(betAmount.value);
-        message.innerHTML = "You won!";
-    } else {
-        balance -= parseInt(betAmount.value);
-        message.innerHTML = "You lost.";
+    if (betSelection.value === flipResult && betAmount.value !== "") {
+      balance += parseInt(betAmount.value);
+      message.innerHTML = "You won!";
+    }
+    else if(betAmount.value !== ""){
+      balance -= parseInt(betAmount.value);
+      message.innerHTML = "You lost.";
     }
     balanceDisplay.innerHTML = balance;
     betAmount.value = "";
     betSelection.value = "";
-    flipButton.disabled = true;
 });
